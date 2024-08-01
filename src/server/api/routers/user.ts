a1a98cd420a4df5f72566907881109fb37907959
Piotr Kuncy <piotr.kuncy@neuca.pl>
2024-08-01 09:49:05 +0200

    refactor(trpc): remove unnceseary complexity

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,10 +1,50 @@
-import { get } from "@/server/api/procedures/user/get";
-import { getSession } from "@/server/api/procedures/user/getSession";
-import { signIn } from "@/server/api/procedures/user/signIn";
-import { createTRPCRouter } from "@/server/api/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "@/server/api/trpc";
+import { generate } from "random-words";
 
 export const userRouter = createTRPCRouter({
-  signIn,
-  getSession,
-  get,
+  signIn: protectedProcedure.query(({ ctx }) => {
+    return ctx.db.user.upsert({
+      where: {
+        id: ctx.session.user.id,
+        // prevent unnecessary updates
+        OR: [
+          {
+            lastSeenAt: {
+              lte: new Date(new Date().getTime() - 1000 * 60 * 60 * 1),
+            },
+          },
+          {
+            lastSeenAt: null,
+          },
+        ],
+      },
+      // we update only email, picutre, and lastSeenAt date as this procedure is called on every signIn
+      update: {
+        email: ctx.session.user.email,
+        picture: ctx.session.user.picture,
+        lastSeenAt: new Date(),
+      },
+      create: {
+        id: ctx.session.user.id,
+        fullname: ctx.session.user.given_name,
+        email: ctx.session.user.email,
+        picture: ctx.session.user.picture,
+        username:
+          `${ctx.session.user.given_name}${generate({ maxLength: 5 }) as string}`.toLocaleLowerCase(),
+      },
+    });
+  }),
+  get: publicProcedure.query(({ ctx }) => {
+    if (ctx.session === null) return null;
+    if (ctx.session.user === null) return null;
+    return ctx.db.user.findUnique({
+      where: {
+        id: ctx.session.user.id,
+      },
+    });
+  }),
 });
