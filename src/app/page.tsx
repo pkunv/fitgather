01f6600d1249a78a069350e09b6fcf45b7fb7be5
Piotr Kuncy <kuncypiotr@gmail.com>
2024-08-10 10:26:28 +0200

    feat: newest outfits on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { OutfitForm } from "@/components/outfit/outfit-form";
-import { TypographyH1 } from "@/components/ui/typography";
+import { OutfitList } from "@/components/outfit/outfit-list";
+import { Spinner } from "@/components/ui/spinner";
+import { TypographyH1, TypographyH2 } from "@/components/ui/typography";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Suspense } from "react";
 
 export default async function Home() {
   const { getUser } = getKindeServerSession();
@@ -10,6 +13,10 @@ export default async function Home() {
     <>
       <TypographyH1>Let&apos;s make an outfit!</TypographyH1>
       <OutfitForm user={user} action="create" />
+      <TypographyH2>Newest outfits</TypographyH2>
+      <Suspense fallback={<Spinner />}>
+        <OutfitList type="newest" />
+      </Suspense>
     </>
   );
 }
