98f1d667a30adca42c5b376055da159e02c3b09a
Piotr Kuncy <kuncypiotr@gmail.com>
2025-11-02 18:08:41 +0100

    fix: eslint error

--- src/lib/use-debounce.ts
+++ src/lib/use-debounce.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export function useDebounce<T>(value: T, delay: number = 300): T {
+export function useDebounce<T>(value: T, delay = 300): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
@@ -15,4 +15,3 @@ export function useDebounce<T>(value: T, delay: number = 300): T {
 
   return debouncedValue;
 }
-
